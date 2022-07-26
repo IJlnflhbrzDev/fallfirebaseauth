20f2fc59d555aeddef6d4a368c48ce8e890035c9
unknown <ijlnflhbrzdev@gmail.com>
2022-07-26 17:07:22 +0700

    +[UPDATE] FITUR LOG OUT AND SHOW EMAIL YG BARU LOGIN

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,12 +6,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: const Center(
-        child: Text('Home Page View'),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Signed in as'),
+            Text(user.email!),
+            const SizedBox(height: 10),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+              ),
+              icon: const Icon(Icons.arrow_back),
+              label: const Text('Sigh In'),
+              onPressed: () => FirebaseAuth.instance.signOut(),
+            )
+          ],
+        ),
       ),
     );
   }
