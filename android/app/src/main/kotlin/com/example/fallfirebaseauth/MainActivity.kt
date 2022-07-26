485dbe1f23ae8b2e9060a0c621a01a7c1bc6ca47
unknown <ijlnflhbrzdev@gmail.com>
2022-07-26 16:52:07 +0700

    +[UPDATE] Inital Commit Auth

---
+++ android/app/src/main/kotlin/com/example/fallfirebaseauth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fallfirebaseauth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/signin/signin.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: dead_code, unused_element
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+              ),
+              icon: const Icon(Icons.lock_open),
+              label: const Text('Sigh In'),
+              onPressed: signInAuth,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future signInAuth() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    );
+  }
+}
